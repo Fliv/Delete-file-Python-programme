263be27fb976ded00c71f025c9291867eee3e45d
Living <415071280@qq.com>
2017-03-26 23:43:42 +0800

    增加删除功能

--- DeleteDocInTime.py
+++ DeleteDocInTime.py
@@ -1,36 +1,48 @@
-import os,datetime
+import datetime
+import os
 
-BASE_DIR = "F:/1"
-DELETE_DAYS = 300
-#days
+BASE_DIR = "D:\downloads"
+DELETE_DAYS = 270
+# days
 
-#list documents in dir
+# list documents in dir
 dir_list = os.listdir(BASE_DIR)
 doc_list = []
+delete_list = []
 for doc in dir_list:
-	doc_path = os.path.join(BASE_DIR,doc)
-	if os.path.isfile(doc_path):
-		doc_list.append(doc_path)
+    doc_path = os.path.join(BASE_DIR, doc)
+    if os.path.isfile(doc_path):
+        doc_list.append(doc_path)
 
-
-#delete overtime files
+# delete overtime files
 now_time = datetime.date.today()
 delta_day = datetime.timedelta(days=DELETE_DAYS)
 delete_time = now_time - delta_day
 
 all_num = len(doc_list)
 delete_num = 0
-rest_num = 0
-for doc in doc_list:
-	doc_date = datetime.date.fromtimestamp(os.path.getmtime(doc))
-	#print doc, doc_date < delete_time
-	if doc_date < delete_time:
-		print "Delete: " + doc 
-		delete_num += 1
-rest_num = all_num - delete_num
 
+for doc in doc_list:
+    doc_date = datetime.date.fromtimestamp(os.path.getmtime(doc))
+    # print doc, doc_date < delete_time
+    if doc_date < delete_time:
+        delete_list.append(doc)
+        print "Delete: " + doc.decode("gbk")
+        delete_num += 1
+print delete_num, "files to be delete"
+
+delete_num = 0
+answer = raw_input("Delete all these files?")
+if answer.upper() == "Y" or answer == "YES":
+    for doc in delete_list:
+        try:
+            os.remove(doc)
+            print "Deleted: " + doc.decode("gbk")
+            delete_num += 1
+        except:
+            pass
 
 print now_time
 print "There are ", all_num, " files"
-print "delete: ", delete_num, " files"
-print "left: ", rest_num, " files"
+print "Deleted: ", delete_num, " files"
+print "Left: ", all_num-delete_num, " files"
